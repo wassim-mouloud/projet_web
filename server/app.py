b69e222dcd98b01bd0da0975e3f39cc388efd1a4
wassim-mouloud <simo.rko10@gmail.com>
2024-04-12 13:57:03 +0200

    creating the db

--- server/app.py
+++ server/app.py
@@ -1,8 +1,19 @@
 from flask import Flask
 from models import db
+from config import ApplicationConfig
 
 app = Flask(__name__)
+app.config.from_object(ApplicationConfig)
 
 db.init_app(app)
-db.create_all()
 
+with app.app_context() :
+    db.create_all()
+    
+@app.route("/register")
+def register_user() :
+    pass
+
+if __name__ == "__main__" :
+    app.run(debug= True)
+    
--- server/config.py
+++ server/config.py
@@ -1,4 +1,14 @@
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+
 class ApplicationConfig :
-    SQLALCHEMY_TRACK_MODIFICATIONS = FALSE
+    
+    SECRET_KEY = os.environ["SECRET_KEY"]
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = r"sqlite:///./db.sqlite"
