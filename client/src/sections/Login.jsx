569a85bb75601601fed4b3f564485ffaa4741394
wassim-mouloud <simo.rko10@gmail.com>
2024-04-13 12:52:26 +0200

    adding client login function

--- client/src/sections/Login.jsx
+++ client/src/sections/Login.jsx
@@ -1,29 +1,40 @@
 import React, { useEffect, useState } from 'react'
+import httpClient from '../httpClient'
 
 function Login() {
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    const login = async ()=>{
+    const loginUser = async ()=>{
+      const resp = await httpClient.post("//localhost:8000/login" , { email, password })
 
+      console.log(resp.data)
     }
+
+    useEffect(()=>{
+      console.log(email)
+    }, [email])
+    useEffect(()=>{
+      console.log(password)
+    }, [password])
 
 
   return (
     <div className='' >
-        <div className='w-screen h-[10vh] flex justify-start items-center p-4 md:mt-4 md:ml-4' >
+        {/* <div className='w-screen h-[10vh] flex justify-start items-center p-4 md:mt-4 md:ml-4' >
             <img src="/images/disney-plus-logo.png" alt="" className='h-12 md:h-16' />
-        </div>
-        <div className='h-[90vh] w-screen flex flex-col justify-center items-center gap-6' >
-                <input onChange={(e)=> setEmail(e.target.value)} type="text" placeholder='E-mail' className='bg-[#31333e] text-[18px] md:text-[20px] rounded-md h-[55px] w-[80vw] sm:w-[380px] md:w-[400px] p-5 text-white outline-gray-400 outline-[1px]' />
-                <input onChange={(e)=> setPassword(e.target.value)} type="password" placeholder='Password' className='bg-[#31333e] text-[18px] md:text-[20px] rounded-md h-[55px] w-[80vw] sm:w-[380px] md:w-[400px]  p-5 text-white outline-gray-400 outline-[1px]' />
-                <button onClick={login} className='flex justify-center items-center text-white text-[18px] md:text-[20px] rounded-md h-[55px] w-[80vw] md:w-[400px] sm:w-[380px]  p-5 bg-[#0072d2] font-normal' >Login</button>
+        </div> */}
+        <form className='h-[100vh] w-screen flex flex-col justify-center items-center gap-6' >
+                <img src="/images/disney-plus-logo.png" alt="" className='w-[300px] md:w-[400px]'/>
+                <input onChange={(e)=> setEmail(e.target.value)} type="text" placeholder='E-mail' className='bg-[#31333e] text-[18px] md:text-[20px] rounded-md h-[55px] w-[80vw] sm:w-[380px] md:w-[450px] lg:w-[500px] p-5 text-white outline-gray-400 outline-[1px]' />
+                <input onChange={(e)=> setPassword(e.target.value)} type="password" placeholder='Password' className='bg-[#31333e] text-[18px] md:text-[20px] rounded-md h-[55px] w-[80vw] sm:w-[380px] md:w-[450px] lg:w-[500px]  p-5 text-white outline-gray-400 outline-[1px]' />
+                <button type='submit' onClick={loginUser} className='flex justify-center items-center text-white text-[18px] md:text-[20px] rounded-md h-[55px] w-[80vw] md:w-[450px] sm:w-[380px] lg:w-[500px]  p-5 bg-[#0072d2] font-normal' >Login</button>
                 {/* <button onClick={googleLogin}  className='flex justify-center items-center gap-2 text-white text-[18px] md:text-[20px] rounded-md h-[55px] w-[80vw] md:w-[400px] sm:w-[380px]  p-5 bg-[#0072d2] font-normal' >
                     <img src="/images/google.png" alt="" className='w-8 h-8' />
                     <p>Login with Google</p>
                 </button> */}
-        </div>
+        </form>
     </div>
   )
 }
