fbd2c1d34771779090e28ea19a8291d5d435ce35
wassim-mouloud <simo.rko10@gmail.com>
2024-07-05 22:57:56 +0200

    changing redis connection

--- server/config.py
+++ server/config.py
@@ -15,4 +15,4 @@ class ApplicationConfig :
     SESSION_TYPE = "redis"
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
-    SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
+    SESSION_REDIS = redis.from_url("redis://:yGw2CABKxuSEHD5gxlwFmqmDEAhpTYsM@redis-18483.c293.eu-central-1-1.ec2.redns.redis-cloud.com:18483/0")
