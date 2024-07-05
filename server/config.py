a1346e06d9851d06cba26d6adacac9fd43b10ef6
wassim-mouloud <simo.rko10@gmail.com>
2024-07-05 22:23:27 +0200

    redis

--- server/config.py
+++ server/config.py
@@ -5,13 +5,14 @@ import redis
 load_dotenv()
 
 
-class ApplicationConfig:
+class ApplicationConfig :
+    
     SECRET_KEY = os.environ["SECRET_KEY"]
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = r"sqlite:///./db.sqlite"
-    SESSION_TYPE = "filesystem"  
+    SESSION_TYPE = "redis"
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
-    SESSION_FILE_DIR = "./flask_session/"
+    SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
