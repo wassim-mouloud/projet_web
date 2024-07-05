07c8cc7693da17590a6bbb4338187d7dbe83cc33
wassim-mouloud <simo.rko10@gmail.com>
2024-07-05 21:58:54 +0200

    removing redis

--- server/config.py
+++ server/config.py
@@ -5,14 +5,13 @@ import redis
 load_dotenv()
 
 
-class ApplicationConfig :
-    
+class ApplicationConfig:
     SECRET_KEY = os.environ["SECRET_KEY"]
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = r"sqlite:///./db.sqlite"
-    SESSION_TYPE = "redis"
+    SESSION_TYPE = "filesystem"  
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
-    SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
+    SESSION_FILE_DIR = "./flask_session/"
